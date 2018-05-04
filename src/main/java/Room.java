b44bdb43134b07937ff934acd617e8945e3bc563
Edward Fallon <eddie_fallon@hotmail.com>
2018-05-04 13:46:47 +0100

    created absract class Room

---
+++ src/main/java/Room.java
@@ -0,0 +1,11 @@
+public abstract class Room {
+    private int capacity;
+
+    public Room(int capacity){
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
